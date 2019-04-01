47a316d595e011a93c68d726e8a99fd947527c5a
Marcio <marcio.i.silva@outlook.com>
2019-04-01 13:36:09 -0300

    adicionando comentarios
    adicionando comentarios

---
+++ operadores.R
@@ -0,0 +1,78 @@
+
+#operadores basicos
+#operadores matematicos
+
+#adição
+7 +7
+
+
+#subtraçao
+7 -4
+
+
+#multiplicação
+5 * 5
+
+
+#divisão
+6 / 6
+
+
+#potenciação
+3^2
+3 ** 3
+
+
+#resto da divisão
+16 %% 3
+ 
+
+
+#operadores relacionais
+
+
+#salvando numeros em variaveis
+x=7
+y=5
+
+
+
+
+
+#operadores logicos
+#comparando
+x>8
+y=5
+
+x<8
+x<=8
+x>=8
+x==8
+x!=8
+
+
+
+
+
+
+#operador logico 
+
+#and ("e"= &)
+
+(x==8)&(x==6)
+(x==7)&(x==5)
+(x==8)&(x==5)
+
+
+#or ("ou" = |)
+(x==8) |(x>5)
+(x==8) |(x>=5)
+  
+
+
+#not (não)
+x >8
+
+
+#negando a negacão
+print (!x>8)
---
+++ variaveis.R
@@ -0,0 +1,89 @@
+
+
+#criando variavel
+
+
+#criando variavel
+var1=100
+#exibindo
+var1
+
+#verificando tipo de variavel
+mode(var1)
+
+#caso nao lembre para que ser o mode
+help("mode")
+
+#fazendo raiz quadrada
+sqrt(var1)
+
+#atribuindo o valor de uma variavel em outra
+var2 = var1
+
+
+#exibindo variaveis
+vaR2
+var2
+
+#verificando o tipo da variavel
+mode(var2)
+
+
+#mostra o tipo da variavel, integer, double, etc...
+typeof(var2)
+
+
+#para saber mais usa o help
+help( "typeof")
+
+
+
+#atribuir varios elementos em uma variavel
+
+var3=c("primeiro", "segundo","terceiro")
+var3
+
+
+
+mode(var3)
+
+
+#variavel pode ser função
+var4=function(x){x+3}
+var
+mode(var4)
+
+
+
+#mudar o tipo do dado
+var5=as.character(var1)
+var5
+mode(var5)
+
+
+#atribuindo valores
+x <- c(1,2,3)
+x
+c(4,5,6) ->y
+y
+
+#tambem pode ser feito desse modo
+assing("x", c(7.8,9,-4))
+x
+
+
+#chamando apenas um unico item 
+x[1]
+
+
+#exibir os objetos
+ls()
+
+
+#exibir objetos
+objects()
+
+
+#remover objetos
+rm(x)
+x
